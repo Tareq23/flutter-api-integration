b3facdca2aeaa6dfd9f48a2d3e1c9284ecef8f19
Tareq23 <mdtarequlislam634@gmail.com>
2021-12-27 11:31:54 +0600

    dependencie add flutter_staggered_grid_view: ^0.4.1

---
+++ lib/pages/home/widgets/book_staggered_grid_view.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class BookStaggerGridView extends StatefulWidget {
+  const BookStaggerGridView({Key? key}) : super(key: key);
+
+  @override
+  _BookStaggerGridViewState createState() => _BookStaggerGridViewState();
+}
+
+class _BookStaggerGridViewState extends State<BookStaggerGridView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
