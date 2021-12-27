826c5970641f2ebf921fd47da3609bf236d55af6
Tareq23 <mdtarequlislam634@gmail.com>
2021-12-27 11:44:10 +0600

    add Book class model

---
+++ lib/model/book.dart
@@ -0,0 +1,31 @@
+
+class Book
+{
+  String type;
+  String name;
+  String publisher;
+  DateTime date;
+  String imgUrl;
+  num score;
+  num rating;
+  num height;
+  String review;
+  Book(
+      this.type,
+      this.name,
+      this.publisher,
+      this.date,
+      this.imgUrl,
+      this.score,
+      this.rating,
+      this.height,
+      this.review,
+      );
+
+  static List<Book> generateBookList (){
+    return [
+      
+    ];
+  }
+
+}
